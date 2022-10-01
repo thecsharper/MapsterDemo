a50b8678920e7e80ad2e0f65cb064ce1cfde1584
thecsharper <si@thecsharper.com>
2022-10-01 16:08:36 +0100

    Updated tests with null check

--- MapsterDemoTests/MapsterTests.cs
+++ MapsterDemoTests/MapsterTests.cs
@@ -14,19 +14,19 @@ namespace MapsterDemoTests
         {
             var destination = new MapsterMap().MapTypes();
 
-            destination.Id.Should().Be(1);
+            destination!.Id.Should().Be(1);
             destination.Name.Should().Be("Test Name");
             DateTime.TryParse(destination.CreatedAt, out DateTime dt);
             dt.Should().BeCloseTo(DateTime.UtcNow, 3.Seconds());
             destination.Email.Should().Be("test@test.com");
             destination.IsActive.Should().BeTrue();
-            destination.PostalAddress.AddressLine1.Should().Be("AddressLine1");
+            destination.PostalAddress!.AddressLine1.Should().Be("AddressLine1");
             destination.PostalAddress.AddressLine2.Should().Be("AddressLine2");
             destination.PostalAddress.City.Should().Be("City");
             destination.PostalAddress.Country.Should().Be("Country");
             destination.PostalAddress.State.Should().Be("State");
             destination.PostalAddress.ZipCode.Should().Be("ZipCode");
-            destination.PostalAddress.BillingAddress.AddressLine1.Should().Be("Billing1");
+            destination.PostalAddress.BillingAddress!.AddressLine1.Should().Be("Billing1");
         }
     }
 }
