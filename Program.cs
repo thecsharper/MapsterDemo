64ed3026cc939da38f067fb239eae470509d6f88
thecsharper <si@thecsharper.com>
2022-10-01 16:09:49 +0100

    Added null check to demo

--- Program.cs
+++ Program.cs
@@ -2,15 +2,15 @@
 
 var destination = new MapsterMap().MapTypes();
 
-Console.WriteLine(destination.Id);
+Console.WriteLine(destination!.Id);
 Console.WriteLine(destination.Name);
 Console.WriteLine(destination.CreatedAt);
 Console.WriteLine(destination.Email);
 Console.WriteLine(destination.IsActive);
-Console.WriteLine(destination.PostalAddress.AddressLine1);
+Console.WriteLine(destination.PostalAddress!.AddressLine1);
 Console.WriteLine(destination.PostalAddress.AddressLine2);
 Console.WriteLine(destination.PostalAddress.City);
 Console.WriteLine(destination.PostalAddress.Country);
 Console.WriteLine(destination.PostalAddress.State);
 Console.WriteLine(destination.PostalAddress.ZipCode);
-Console.WriteLine(destination.PostalAddress.BillingAddress.AddressLine1);
+Console.WriteLine(destination.PostalAddress.BillingAddress!.AddressLine1);
